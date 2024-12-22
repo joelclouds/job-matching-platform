3be1ec50d87e5ad3cdf95773e0a7f221d51e2129
joelclouds <91001875+joelclouds@users.noreply.github.com>
2024-12-22 17:33:02 +0000

    Create meeting.js

---
+++ models/meeting.js
@@ -0,0 +1,11 @@
+class Meeting {
+  constructor(id, title, date, time, attendees) {
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.time = time;
+    this.attendees = attendees;
+  }
+}
+
+module.exports = Meeting;
